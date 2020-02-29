170934a8025a95b28ce6229d6752f0c9c0e013cf
Luke Nelson <lukenels@cs.washington.edu>
2020-02-29 10:56:44 -0800

    Update rv64

--- rv64-linux/run.sh
+++ rv64-linux/run.sh
@@ -11,5 +11,5 @@ $QEMU/riscv64-softmmu/qemu-system-riscv64 \
    -drive file=$(dirname $0)/rootfs.ext2,format=raw,id=hd0 \
    -device virtio-blk-device,drive=hd0 \
    -netdev user,id=net0 -device virtio-net-device,netdev=net0 \
-   -fsdev local,security_model=passthrough,id=fsdev0,path=$LINUX/lib \
+   -fsdev local,security_model=passthrough,id=fsdev0,path=$LINUX \
    -device virtio-9p-pci,id=fs0,fsdev=fsdev0,mount_tag=hostshare
