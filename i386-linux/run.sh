2452a5e01b77c6e7cf68867ff37cdc12c3748a9c
Luke Nelson <lukenels@cs.washington.edu>
2020-03-05 11:33:14 -0800

    Update i386 linux configuration

--- i386-linux/run.sh
+++ i386-linux/run.sh
@@ -8,6 +8,5 @@ $QEMU/i386-softmmu/qemu-system-i386 \
 	-kernel $LINUX/arch/x86/boot/bzImage \
 	-initrd $(dirname $0)/rootfs.cpio \
 	-serial mon:stdio \
-	-fsdev local,security_model=passthrough,id=fsdev0,path=$HOME \
+	-fsdev local,security_model=passthrough,id=fsdev0,path=$LINUX \
 	-device virtio-9p-pci,id=fs0,fsdev=fsdev0,mount_tag=hostshare \
-	-enable-kvm
