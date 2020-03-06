bd6735b24eee548c10f6990b365b02a8ce02d534
Luke Nelson <lukenels@cs.washington.edu>
2020-03-05 16:18:18 -0800

    update

--- i386-debian/run.sh
+++ i386-debian/run.sh
@@ -3,10 +3,11 @@ qemu-system-i386 \
 	-nographic \
 	-kernel ~/repo/linux/arch/x86/boot/bzImage \
 	-hda snapshot.img \
-	-append "console=ttyS0 root=/dev/sda rw" \
 	-enable-kvm \
-	-fsdev local,security_model=passthrough,id=fsdev0,path=$HOME \
+	-append "console=ttyS0 root=/dev/sda rw" \
+	-fsdev local,security_model=mapped,id=fsdev0,path=$HOME/repo/linux \
 	-device virtio-9p-pci,id=fs0,fsdev=fsdev0,mount_tag=hostshare \
+	-nic user,model=e1000 \
 	-m 2048m
 
 
