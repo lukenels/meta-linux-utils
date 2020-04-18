d3734eeb386fd162d08c1a42f811fc5e1992aa65
Luke Nelson <luke.r.nels@gmail.com>
2020-04-18 15:15:17 -0700

    x

---
+++ amd64-fedora/build.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+sudo virt-builder \
+  --arch x86_64 \
+  --ssh-inject root:file:$HOME/.ssh/id_ed25519.pub \
+  --size 20G --format qcow2 --output disk.qcow2 \
+  fedora-31
+
+sudo chown $(whoami) disk.qcow2
---
+++ amd64-fedora/run.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+. $(dirname $0)/../config.sh
+
+${QEMU}/x86_64-softmmu/qemu-system-x86_64 \
+	-nographic \
+	-kernel ~/repo/linux/arch/x86/boot/bzImage \
+	-hda disk.qcow2 \
+	-enable-kvm \
+	-append "console=ttyS0 root=/dev/sda4 rw" \
+	-fsdev local,security_model=mapped,id=fsdev0,path="${LINUX}" \
+	-device virtio-9p-pci,id=fs0,fsdev=fsdev0,mount_tag=hostshare \
+	-nic user,model=e1000,hostfwd=tcp::"${CONFIG_SSH_PORT}"-:22  \
+	-m 4G
+
+
