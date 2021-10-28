4413ce8404816e1659bdc88dfe840804934ac736
Luke Nelson <luke.r.nels@gmail.com>
2021-10-28 12:21:06 -0700

    x

--- amd64-fedora/run.sh
+++ amd64-fedora/run.sh
@@ -6,7 +6,7 @@ qemu-system-x86_64 \
 	-kernel ~/repo/linux/arch/x86/boot/bzImage \
 	-hda disk.qcow2 \
 	-enable-kvm \
-	-append "console=ttyS0 root=/dev/sda4 rw" \
+	-append "console=ttyS0 root=/dev/sda3 rw" \
 	-fsdev local,security_model=mapped,id=fsdev0,path="${LINUX}" \
 	-device virtio-9p-pci,id=fs0,fsdev=fsdev0,mount_tag=hostshare \
 	-nic user,model=e1000,hostfwd=tcp::"${CONFIG_SSH_PORT}"-:22  \
